b2e00f786766971195ff683d3ed0d40f24a2271b
TimekillerTK <erwartungen@protonmail.com>
2023-01-03 21:59:12 +0100

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,20 @@ pub mod functions {
     };
     use core::fmt;
     use std::{env, fmt::Display};
+    use std::path::Path;
+    use std::fs;
+    use anyhow::anyhow;
 
     #[derive(Debug, Serialize, Deserialize)]
     struct ResponseTVShow {
-        results: Vec<TVShow>
+        results: Vec<TVShow>,
+        total_results: u32,
+        total_pages: u32
     }
 
     #[derive(Debug, Serialize, Deserialize)]
     struct ResponseSeasons {
-        seasons: Vec<Seasons>
+        seasons: Vec<Season>
     }
     
     #[derive(Debug, Serialize, Deserialize)]
@@ -26,23 +31,43 @@ pub mod functions {
 
     impl Display for TVShow {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "ID: {:7}, Name: {}",
+            write!(f, "ID: {:6}, Name: {}",
             self.id, self.name)
         }
 
     }
     
     #[derive(Debug, Serialize, Deserialize)]
-    pub struct Seasons {
+    pub struct Season {
         pub id: i32,
         pub name: String,
-        pub season_number: i32,
-        pub episode_count: i32
+        pub season_number: u32,
+        pub episode_count: u32
+    }
+
+    impl Display for Season {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "ID: {:6}, Season: {} ({}), Eps: {}",
+            self.id, self.name, self.season_number, self.episode_count)
+        }
+
+    }
+
+    #[derive(Debug)]
+    pub struct VideoFiles<'a> {
+        pub path: &'a Path,
+        pub file_names: Vec<String>
     }
 
 
-    pub async fn search_tv_show(client: Client, show_name: &str) -> Result<Vec<TVShow>, Box<dyn std::error::Error>> {
+    pub async fn search_tv_show(show_name: &str) -> Result<Vec<TVShow>, Box<dyn std::error::Error>> {
+        let response_tv_show = query_moviedb_tvshow(show_name).await?;
+        display_tvshow_response(&response_tv_show);
+        Ok(response_tv_show.results)
+    }
 
+    async fn query_moviedb_tvshow(show_name: &str) -> Result<ResponseTVShow, Box<dyn std::error::Error>> {
+        let client = Client::new();
         let api_key = env::var("API_KEY").unwrap();
         let mut headers = HeaderMap::new();
 
@@ -61,12 +86,25 @@ pub mod functions {
             .await?;
 
         let response: ResponseTVShow = serde_json::from_str(&resp)?;
-        
-        Ok(response.results)
+        Ok(response)
     }
 
-    pub async fn get_tv_show_seasons(client: Client, show_id: i32) -> Result<Vec<Seasons>, Box<dyn std::error::Error>> {
+    fn display_tvshow_response(response: &ResponseTVShow)  {
+        match response.total_results {
+            0                   => println!("No results found!"),
+            1                   => println!("{}", response.results[0]),
+            2_u32..=u32::MAX    => {
+                for show in &response.results {
+                    println!("{}", show);
+                }
+            },
+        }
+    }
 
+
+    pub async fn get_tv_show_seasons(show_id: i32) -> anyhow::Result<Vec<Season>> {
+
+        let client = Client::new();
         let api_key = env::var("API_KEY").unwrap();
         let mut headers = HeaderMap::new();
 
@@ -85,6 +123,46 @@ pub mod functions {
 
         let response: ResponseSeasons = serde_json::from_str(&resp)?;
 
-        Ok(response.seasons)
+        match response.seasons.len() {
+            0 => {
+                Err(anyhow!("No results found!"))
+            },
+            1..=usize::MAX => {
+                for season in &response.seasons {
+                    println!("{}", season);
+                }
+                Ok(response.seasons)
+            }
+            _ => Err(anyhow!("Unknown error!"))
+        }
+        // Ok(response.seasons)
     }
+
+    pub fn list_media_files(path: &str) -> VideoFiles {
+
+        let valid_extensions = [".mkv", ".mp4", ".avi", ".mov"];
+        let directory_path = Path::new(path);
+        let mut video_files: VideoFiles = VideoFiles { path: directory_path, file_names: Vec::new()};
+        
+        let target_dir = fs::read_dir(directory_path)
+            .unwrap();
+
+        for file in target_dir {
+            let file = file.unwrap();
+            let path = file.path();
+            if path.is_file() {
+                let file_name = path.file_name().unwrap().to_str().unwrap();
+                for extension in valid_extensions {
+                    if file_name.ends_with(extension) {
+                        video_files.file_names.push(file_name.to_string());
+                    }
+                }
+            }
+        }
+    
+        video_files.file_names.sort_by(|x, y| x.cmp(y));
+        video_files
+
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,52 @@
 // curl --request GET   --url 'https://api.themoviedb.org/4/search/tv?query=dragon'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
 // curl --request GET   --url 'https://api.themoviedb.org/3/tv/61709'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
 
-use reqwest::Client;
-use fix_show_name::functions::{search_tv_show, get_tv_show_seasons};
+use fix_show_name::functions::{search_tv_show, get_tv_show_seasons, list_media_files};
 use tokio;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let show_name = "dragon ball";
-    let show_id = 61709;
-
-    let client = Client::new();
+    let path = "testpath";
     
-    // Search TV Shows
-    let shows = search_tv_show(client, show_name).await?;
+    // Search for TV Show
+    let tv_shows = search_tv_show(show_name).await?;
+    let selected_tv_show = 61459;
 
-    // TODO: Should display a list of results of show + showid and the choice of picking the right option
-    for show in shows {
-        println!("{}", show)
+    // Display TV Show seasons
+    let seasons = get_tv_show_seasons(selected_tv_show).await?;
+    let selected_season = 62946;
+
+    
+    // List media files in directory
+    let media_files = list_media_files(path);
+    // dbg!(media_files); 
+
+    // if 
+    for (i, file) in media_files.file_names.iter().enumerate() {
+        
+
+        let new_file_name = format!("{}{}", file, "xxxx");
+        
+        let old_path = media_files.path.join(&file);
+        let new_path = media_files.path.join(&new_file_name);
+        println!("{} --> {}", file, new_file_name);
+        // println!("{:?} --> {:?}", old_path, new_path);
+        // fs::rename(old_path, new_path).unwrap();
     }
 
-    
-    // dbg!(shows);
-    
-    // let client = Client::new();
-    // // Get season & episode information about TV Show
-    // let show_details = get_tv_show_seasons(client, show_id).await?;
+    // Target name: 
+    // SHOWNAME - S1 - 001.mkv
 
+    // TODO: Handle cases:
+    // show has >   9 episodes =   0X
+    // show has >  99 episodes =  00X
+    // show has > 999 episodes = 000X
 
     Ok(())
 
 }
+
 
 
