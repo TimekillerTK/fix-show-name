3d743e8a34ccfa77047891d2caea7cfc5f82dc2b
TimekillerTK <erwartungen@protonmail.com>
2023-01-03 08:29:39 +0100

    some progress

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,42 @@ pub mod functions {
         header::{HeaderMap, HeaderValue, AUTHORIZATION},
         Client
     };
-    use std::env;
+    use core::fmt;
+    use std::{env, fmt::Display};
 
     #[derive(Debug, Serialize, Deserialize)]
-    struct Response {
+    struct ResponseTVShow {
         results: Vec<TVShow>
     }
 
     #[derive(Debug, Serialize, Deserialize)]
+    struct ResponseSeasons {
+        seasons: Vec<Seasons>
+    }
+    
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct TVShow {
         pub id: i32,
         pub name: String,
-        pub overview: String
+        pub overview: String,
     }
+
+    impl Display for TVShow {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "ID: {:7}, Name: {}",
+            self.id, self.name)
+        }
+
+    }
+    
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Seasons {
+        pub id: i32,
+        pub name: String,
+        pub season_number: i32,
+        pub episode_count: i32
+    }
+
 
     pub async fn search_tv_show(client: Client, show_name: &str) -> Result<Vec<TVShow>, Box<dyn std::error::Error>> {
 
@@ -37,12 +60,12 @@ pub mod functions {
             .text()
             .await?;
 
-        let response: Response = serde_json::from_str(&resp)?;
+        let response: ResponseTVShow = serde_json::from_str(&resp)?;
         
         Ok(response.results)
     }
 
-    pub async fn get_tv_show_info(client: Client, show_id: i32) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn get_tv_show_seasons(client: Client, show_id: i32) -> Result<Vec<Seasons>, Box<dyn std::error::Error>> {
 
         let api_key = env::var("API_KEY").unwrap();
         let mut headers = HeaderMap::new();
@@ -53,15 +76,15 @@ pub mod functions {
         // Make the GET request with the headers
         // TODO: Needs to handle getting extra pages
         let resp = client
-            .get(format!("https://api.themoviedb.org/4/tv/{}", show_id))
+            .get(format!("https://api.themoviedb.org/3/tv/{}", show_id))
             .headers(headers)
             .send()
             .await?
             .text()
             .await?;
 
-        // let response: Response = serde_json::from_str(&resp)?;
-        dbg!(resp);
-        Ok(())
+        let response: ResponseSeasons = serde_json::from_str(&resp)?;
+
+        Ok(response.seasons)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@
 // GET REQUEST EXAMPLES:
 // curl --request GET   --url 'https://api.themoviedb.org/4/list/1'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
 // curl --request GET   --url 'https://api.themoviedb.org/4/search/tv?query=dragon'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
-// curl --request GET   --url 'https://api.themoviedb.org/4/tv/61709'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
+// curl --request GET   --url 'https://api.themoviedb.org/3/tv/61709'   --header "Authorization: Bearer ${API_KEY}"   --header 'Content-Type: application/json;charset=utf-8'
 
 use reqwest::Client;
-use fix_show_name::functions::{search_tv_show, get_tv_show_info};
+use fix_show_name::functions::{search_tv_show, get_tv_show_seasons};
 use tokio;
 
 
@@ -19,14 +19,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let show_id = 61709;
 
     let client = Client::new();
-
+    
     // Search TV Shows
-    // let shows = search_tv_show(client, show_name).await?;
+    let shows = search_tv_show(client, show_name).await?;
 
-    // Get season & episode information about TV Show
-    let show_details = get_tv_show_info(client, show_id).await?;
+    // TODO: Should display a list of results of show + showid and the choice of picking the right option
+    for show in shows {
+        println!("{}", show)
+    }
 
-    // dbg!(show_details);
+    
+    // dbg!(shows);
+    
+    // let client = Client::new();
+    // // Get season & episode information about TV Show
+    // let show_details = get_tv_show_seasons(client, show_id).await?;
+
 
     Ok(())
 
